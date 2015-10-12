ef636e7c65c3f59abb8c5a48f30271c3c705fc23
Luciekimotho <luciekimotho@gmail.com>
2015-10-12 08:28:29 +0300

    Settings for sending the email form mandrill app

--- Myfirst/settings.py
+++ Myfirst/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.mandrillapp.com'
+EMAIL_HOST_USER = 'luciekimotho@gmail.com'
+EMAIL_HOST_PASSWORD = 'h1sanyCbBEerx3jeHY82TQ'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
